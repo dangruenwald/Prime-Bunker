9a22982b4e80fc3b619b3d7234887692c2be3948
dangruenwald <dangruenwald@outlook.com>
2025-11-01 03:46:08 +0000

    Add files via upload

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,1 +1,12 @@
 
+import Hero from '@/components/Hero'
+import ServiceCards from '@/components/ServiceCards'
+import MapBlock from '@/components/MapBlock'
+
+export default function Page() {
+  return (<>
+    <Hero title="About Prime Bunker" subtitle="Built by traders who still believe in the handshake." />
+    <ServiceCards />
+    <MapBlock />
+  </>)
+}
